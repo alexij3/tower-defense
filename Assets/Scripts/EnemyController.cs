404fa509bd1a2ddacd670d5407279ee977f4aea4
Oleksii <slmk445@gmail.com>
2020-09-22 22:08:57 +0300

    Initial commit

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyController : MonoBehaviour
+{
+    public Transform[] waypoints;
+    public int currentWaypoint;
+
+    public float speed;
+
+    public NavMeshAgent agent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        agent.SetDestination(waypoints[currentWaypoint].position);
+        if (agent.remainingDistance <= .2f)
+        {
+            currentWaypoint++;
+            if (currentWaypoint == waypoints.Length)
+            {
+                Destroy(gameObject);
+                return;
+            }
+            agent.SetDestination(waypoints[currentWaypoint].position);
+        }
+    }
+}
