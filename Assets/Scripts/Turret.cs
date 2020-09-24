9eefd925fb129be87c910317dfa8ff0567d01bb4
Oleksii <slmk445@gmail.com>
2020-09-24 20:55:52 +0300

    Turret and rotation
    -- Added a turret model and created a prefab based on it.
    -- Created a Turret script that handles the finding of a target and rotation towards the target.

---
+++ Assets/Scripts/Turret.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turret : MonoBehaviour
+{
+
+    private Transform target;
+    public float attackRange = 15f;
+    public float rotationSpeed = 10f;
+
+    public string enemyTag = "Enemy";
+
+    public Transform partToRotate;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("UpdateTarget", 0f, 0.5f);
+    }
+
+    void UpdateTarget()
+    {
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag(enemyTag);
+
+        float shortestDistanceToEnemy = Mathf.Infinity;
+        GameObject nearestEnemy = null;
+        foreach (GameObject enemy in enemies)
+        {
+            float distanceToEnemy = Vector3.Distance(transform.position, enemy.transform.position);
+            if (distanceToEnemy < shortestDistanceToEnemy)
+            {
+                shortestDistanceToEnemy = distanceToEnemy;
+                nearestEnemy = enemy;
+            }
+        }
+        if (nearestEnemy != null & shortestDistanceToEnemy <= attackRange)
+        {
+            target = nearestEnemy.transform;
+        } else
+        {
+            target = null;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (target == null)
+        {
+            return;
+        }
+
+        Vector3 lookDirection = target.position - transform.position;
+        Quaternion lookRotation = Quaternion.LookRotation(lookDirection);
+        Vector3 rotation = Quaternion.Lerp(partToRotate.rotation, lookRotation, rotationSpeed * Time.deltaTime).eulerAngles;
+        partToRotate.rotation = Quaternion.Euler(0f, rotation.y, 0f);
+
+
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, attackRange);
+    }
+}
